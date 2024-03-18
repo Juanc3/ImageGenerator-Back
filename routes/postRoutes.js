3d46c9bdac18c1bb066e6955cde80e85d80713e9
Juan <juan.gauna.dev@gmail.com>
2024-03-18 02:40:38 -0300

    feat: add cron and Image remover

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
+import cron from "node-cron";
 
 import PostSchema from "../database/models/post.js";
 
@@ -14,9 +15,31 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+async function cleanImages() {
+  try {
+    const AllPosts = await PostSchema.find().sort({ createdAt: 1 }).exec();
+
+    if (AllPosts.length > 15) {
+      for (let i = 0; i < 3; i++) {
+        const deletedImage = AllPosts[i];
+        await cloudinary.uploader.destroy(deletedImage.public_id);
+        await PostSchema.findByIdAndRemove(deletedImage._id);
+      }
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+cron.schedule("0 0 * * *", () => {
+  console.log("Cleaning Images...");
+  cleanImages();
+});
+
 router.route("/").get(async (req, res) => {
   try {
     const posts = await PostSchema.find({});
+    await cleanImages();
     res.status(200).json({ success: true, data: posts });
   } catch (err) {
     res.status(500).json({
